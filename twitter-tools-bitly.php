3bc8dba124030d52c6a1608a1ec42b89c76e59e2
alexkingorg <alexkingorg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-07 04:03:39 +0000

    adding todo for j.mp
    git-svn-id: http://svn.wp-plugins.org/twitter-tools/trunk@152645 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitter-tools-bitly.php
+++ twitter-tools-bitly.php
@@ -10,6 +10,8 @@ Author URI: http://crowdfavorite.com
 
 // ini_set('display_errors', '1'); ini_set('error_reporting', E_ALL);
 
+// TODO - option for j.mp vs. bit.ly URLs
+
 if (!defined('PLUGINDIR')) {
 	define('PLUGINDIR','wp-content/plugins');
 }
